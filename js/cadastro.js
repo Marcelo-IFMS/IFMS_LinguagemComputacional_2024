692465e49c39ce7fba510111b59766d5793dbb71
Marcelo Tomporoski Perez <marcelo.perez@ifms.edu.br>
2024-04-02 10:09:34 -0400

    cadastro Escreve

--- js/cadastro.js
+++ js/cadastro.js
@@ -11,3 +11,7 @@ function cad(nome, telefone) {
 function escreveCadastro() {
     console.log(cadastro);
 }
+
+function escreveHTML(){
+    document.getElementById('mostrarCadastro').innerHTML = cadastro[0].nome+" - "+cadastro[0].telefone; //[0].nome //constructor.name
+}
